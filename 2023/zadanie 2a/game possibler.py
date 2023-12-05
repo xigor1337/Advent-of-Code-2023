402d26657777d8053f61da66f5a92d27d18317af
Igor <130784433+xigor1337@users.noreply.github.com>
2023-12-05 18:59:22 +0100

    Add files via upload

---
+++ 2023/zadanie 2a/game possibler.py
@@ -0,0 +1,51 @@
+values = open("input values.txt", "r")
+suma = 0
+
+#12 red cubes, 13 green cubes, and 14 blue cubes
+red = 12
+green = 13
+blue = 14
+
+for line in values:
+    line = line.strip()
+    line = line.split(";")
+    games = []
+    for i in line:
+        games.append(i)
+    games[0] = ""
+    line = line[0].split(":")
+    games[0] = line[1]
+    line.pop()
+    #print("games:", games)
+    #print("line:", line)
+    #games to lista z grami
+    #line to lista z numerem gry
+
+    red_count = True
+    green_count = True
+    blue_count = True
+    for game in games:
+        game = game.strip().split(",")
+        #print("game: ", game)
+        zbior_danych = {}
+        for data in game:
+            data = data.strip().split(" ")
+            #print("data: ", data)
+            zbior_danych[data[1]] = data[0]
+        print("zbior_danych: ", zbior_danych)
+        for data in zbior_danych:
+            if data == "red":
+                if int(zbior_danych[data]) > red:
+                    red_count = False
+            elif data == "blue":
+                if int(zbior_danych[data]) > blue:
+                    blue_count = False
+            elif data == "green":
+                if int(zbior_danych[data]) > green:
+                    green_count = False
+        print("red_count:", red_count, "\nblue_count:", blue_count, "\ngreen_count:", green_count)
+    if red_count == True and blue_count == True and green_count == True:
+        line = line[0].split(" ")
+        suma += int(line[1])
+    print(suma)
+print(suma)
---
+++ 2023/zadanie 4b/lottery checker.py
@@ -0,0 +1,40 @@
+#output too low, possible problem that after adding to the end of the list it doesnt` add more
+
+with open("input values.txt", 'r') as file:
+    lines = [file.readline().strip() for _ in range(196)]
+suma = 0
+numer = 1
+karty = [1]
+for _ in range(195):
+    karty.append(1)
+line_number = 0
+for line in lines:
+    line = line.split("|")
+    print(line)
+    numbers = line[1].split(" ")
+    numbers = [item for item in numbers if item]
+    print("numbers:", numbers)
+    winning = line[0].split(":")
+    winning = winning[1].split(" ")
+    winning = [item for item in winning if item]
+    print("winning:", winning)
+
+    matched = 0
+    for _ in range(karty[0 + line_number]):
+        for i in winning:
+            for j in numbers:
+                if i == j:
+                    matched += 1
+    for i in range(matched):
+        if line_number + i + 1 >= len(karty) - line_number:
+            if line_number + i + 1 == len(karty) - line_number:
+                k = i
+            i -= k
+        if line_number + i + 1 < len(karty):
+            karty[line_number + i + 1] += 1
+    print(line_number)
+    line_number += 1
+print(karty)
+for i in karty:
+    suma += i
+print(suma)
